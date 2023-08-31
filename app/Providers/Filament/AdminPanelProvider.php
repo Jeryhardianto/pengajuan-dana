af5a4a8087f7ef7e06d9d420d08a8b93363a78e6
jery hardianto <jery@carigi.id>
2023-09-01 01:47:06 +0700

    Add: procfile

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,7 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
-            ->favicon(url('image/icon-ma.png'))
+            ->favicon(asset('image/icon-ma.png'))
             ->colors([
                 'primary' => Color::Blue,
             ])
